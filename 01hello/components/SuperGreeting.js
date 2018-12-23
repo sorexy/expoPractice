87693531a5e4f446842346cd7a44ea2f66622a82
Richard <richardrother1994@gmail.com>
2018-12-23 19:39:05 +0000

    added Greeting component

---
+++ 01hello/components/SuperGreeting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+// import { Greeting } from './Greeting';
+
+export class SuperGreeting extends React.Components {
+    render() {
+        return (
+            // <Greeting name={this.props.supername1}/>
+            // <Greeting name={this.props.supername2}/>
+            // <Greeting name={this.props.supername3}/>
+        );
+    }
+}
